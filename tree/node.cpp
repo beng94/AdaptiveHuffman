11cadcc2dc04d719484f389ac4d4830fccdd0c5e
Tamás Bence Gedai <gedai.bence@gmail.com>
2017-02-09 20:26:59 +0100

    node: Node functions implemented

--- tree/node.cpp
+++ tree/node.cpp
@@ -1,9 +1,34 @@
 #include "node.hpp"
 
+
+Node* Node::getParent() {
+    return parent;
+}
+
 Node* Node::getLeftChild() {
     return left_child;
 }
 
 Node* Node::getRightChild() {
     return right_child;
+}
+
+bool Node::isLeftChild(Node* node) {
+    return left_child == node;
+}
+
+void Node::swap(Node* node) {
+    if(parent->isLeftChild(this)) {
+        parent->left_child = node;
+    } else {
+        parent->right_child = node;
+    }
+
+    Node* tmpParent = parent;
+    parent = node->parent;
+    node->parent = tmpParent;
+}
+
+Data& Node::getData() {
+    return data;
 }
--- tree/node.hpp
+++ tree/node.hpp
@@ -1,12 +1,18 @@
 #include "../data/data.hpp"
 
-
 class Node {
     private:
+        Data data;
+        Node* parent;
         Node* left_child;
         Node* right_child;
 
+        bool isLeftChild(Node*);
+
     public:
+        Data& getData();
+        Node* getParent();
         Node* getLeftChild();
         Node* getRightChild();
+        void swap(Node*);
 };
