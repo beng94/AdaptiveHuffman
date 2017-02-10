06a3e3745171f986b5c8a2b2a36273d248ca6913
Tamás Bence Gedai <gedai.bence@gmail.com>
2017-02-10 11:05:55 +0100

    main: encoded binary read in

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include "huffman/huffman_encoder.hpp"
+#include "io/input_reader.hpp"
 
 #include <iostream>
 
 int main() {
     HuffmanEncoder encoder("test.txt", "out.txt");
     encoder.encode(); // TODO: It should return a string
+
+    InputReader i("out.txt");
+    while(!i.end()) {
+        std::cout << i.nextBit();
+    }
 }
